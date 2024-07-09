5fe2ef43b6ef0f6a0597638fb0576ba7caa1ff2c
Deepakindorkar <157065028+Deepakindorkar@users.noreply.github.com>
2024-07-09 16:56:43 +0530

    Create main.tf
    New

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+resource "aws_instance" "Demo" {
+  ami           = "ami-0d758c1134823146a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Demo"
+  }
+}
